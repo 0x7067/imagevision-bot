d485155163178a5687f829fcddd44a972ee9299d
0x7067 <moisespedro15@gmail.com>
2017-05-05 00:47:43 -0300

    Switched from Bing to Yandex translator

--- translator.py
+++ translator.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-from BingTranslator import Translator #importing Translator class for translations.
+from yandex_translate import YandexTranslate #importing Translator class for translations.
 
-# Your Bing API credentials here
-client_id = ""
-client_secret = ""
-
-translator = Translator(client_id, client_secret)
+# Your Yandex API credentials here
+translator = YandexTranslate('')
 
 def translate_en_pt(message):
-	phrase_translated = translator.translate(message, "pt") #translating phrase
-	return phrase_translated
+    phrase_translated = translator.translate(message, 'pt') 
+    return phrase_translated
 def translate_en_persian(message):
-	phrase_translated = translator.translate(message, "fa")
-	return phrase_translated
+    phrase_translated = translator.translate(message, 'fa')
+    return phrase_translated
