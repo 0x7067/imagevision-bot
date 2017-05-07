0ba0406bb6ac3b5a082108b08ca89d8c51cf8cf5
Pedro Guimarães <moisespedro15@gmail.com>
2017-05-07 02:54:24 -0300

    Code is now PEP8 compliant.

--- mscomputervision.py
+++ mscomputervision.py
@@ -7,11 +7,11 @@ import time
 # Variables
 
 _msurl = 'https://api.projectoxford.ai/vision/v1.0/analyze'
-_mskey = '' # YOUR Microsoft Computer Vision API token here
+_mskey = ''  # YOUR Microsoft Computer Vision API token here
 _msMaxNumRetries = 10
 
-def msProcessRequest( json, data, headers, params ):
 
+def msProcessRequest(json, data, headers, params):
     """
     Helper function to process the request to Project Oxford
 
@@ -26,34 +26,35 @@ def msProcessRequest( json, data, headers, params ):
 
     while True:
 
-        response = requests.request( 'post', _msurl, json = json, data = data, headers = headers, params = params )
+        response = requests.request(
+            'post', _msurl, json=json, data=data, headers=headers, params=params)
 
-        if response.status_code == 429: 
-            print( "Message: %s" % ( response.json()['error']['message'] ) )
+        if response.status_code == 429:
+            print("Message: %s" % (response.json()['error']['message']))
 
-            if retries <= _msMaxNumRetries: 
-                time.sleep(1) 
+            if retries <= _msMaxNumRetries:
+                time.sleep(1)
                 retries += 1
                 continue
-            else: 
-                print( 'Error: failed after retrying!' )
+            else:
+                print('Error: failed after retrying!')
                 break
 
         elif response.status_code == 200 or response.status_code == 201:
 
-            if 'content-length' in response.headers and int(response.headers['content-length']) == 0: 
-                result = None 
-            elif 'content-type' in response.headers and isinstance(response.headers['content-type'], str): 
-                if 'application/json' in response.headers['content-type'].lower(): 
-                    result = response.json() if response.content else None 
-                elif 'image' in response.headers['content-type'].lower(): 
+            if 'content-length' in response.headers and int(response.headers['content-length']) == 0:
+                result = None
+            elif 'content-type' in response.headers and isinstance(response.headers['content-type'], str):
+                if 'application/json' in response.headers['content-type'].lower():
+                    result = response.json() if response.content else None
+                elif 'image' in response.headers['content-type'].lower():
                     result = response.content
         else:
-            print( "Error code: %d" % ( response.status_code ) )
-            print( "Message: %s" % ( response.json()['error']['message'] ) )
+            print("Error code: %d" % (response.status_code))
+            print("Message: %s" % (response.json()['error']['message']))
 
         break
-        
+
     return result
 
 
--- translator.py
+++ translator.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
-from yandex_translate import YandexTranslate #importing Translator class for translations.
+# importing Translator class for translations.
+from yandex_translate import YandexTranslate
 
 # Your Yandex API credentials here
 translator = YandexTranslate('')
+
 
 def translate_en_pt(message):
     phrase_translated = translator.translate(message, 'pt')
     str = "".join(phrase_translated.get('text'))
     return str
+
+
 def translate_en_persian(message):
     phrase_translated = translator.translate(message, 'fa')
     str = "".join(phrase_translated.get('text'))
